ee8a705c0794a08386de3d0bfe282655a1a90ed9
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-01-02 14:53:52 -0800

    Add leetcode 6_zigzag-conversion.java solution

---
+++ leetcode/algorithms/java/6_zigzag-conversion.java
@@ -0,0 +1,71 @@
+// Source: https://leetcode.com/problems/zigzag-conversion/
+// Author: Tom Murphy https://github.com/murphy-codes/
+// Date: 2025-01-02
+
+/****************************************
+* 
+* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+* (you may want to display this pattern in a fixed font for better legibility)
+* 
+* P   A   H   N
+* A P L S I I G
+* Y   I   R
+* 
+* And then read line by line: "PAHNAPLSIIGYIR"
+* Write the code that will take a string and make this conversion given a number of rows:
+* string convert(string s, int numRows);
+* 
+* Example 1:
+* Input: s = "PAYPALISHIRING", numRows = 3
+* Output: "PAHNAPLSIIGYIR"
+* 
+* Example 2:
+* Input: s = "PAYPALISHIRING", numRows = 4
+* Output: "PINALSIGYAHRPI"
+* Explanation:
+* P     I    N
+* A   L S  I G
+* Y A   H R
+* P     I
+* 
+* Example 3:
+* Input: s = "A", numRows = 1
+* Output: "A"
+* 
+* Constraints:
+* 
+* • 1 <= s.length <= 1000
+* • s consists of English letters (lower-case and upper-case), ',' and '.'.
+* • 1 <= numRows <= 1000
+* 
+****************************************/
+
+class Solution {
+    // To solve in O(n), we'll use an array of StringBuilders for the rows.
+    // We iterate through the input string `s` once, appending per row.  
+    // The direction of traversal will switch at the top or bottom row.
+    // Lastly, we combine all rows into a single string for the zigzag result.
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for(int i=0; i<numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+        boolean descending = true;
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            rows[i].append(c);
+            i += descending ? 1 : -1;
+            if (i == 0 || i == numRows - 1) {
+                descending = !descending;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb : rows) {
+            result.append(sb);
+        }
+        return result.toString();
+    }
+}
