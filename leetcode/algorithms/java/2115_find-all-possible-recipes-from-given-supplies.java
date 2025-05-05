f07a795de7f2ab9c625fb53a32faf3c95fae1610
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-05-04 17:28:39 -0700

    Add leetcode 2115_find-all-possible-recipes-from-given-supplies.java solution

--- leetcode/algorithms/java/2115_find-all-possible-recipes-from-given-supplies.java
+++ leetcode/algorithms/java/2115_find-all-possible-recipes-from-given-supplies.java
@@ -1,12 +1,21 @@
 // Source: https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-01-21
+// At the time of submission:
+//   Runtime 67 ms Beats 40.77%
+//   Memory 47.47 MB Beats 7.63%
 
 /****************************************
 * 
-* You have information about `n` different recipes. You are given a string array `recipes` and a 2D string array `ingredients`. The `i^th` recipe has the name `recipes[i]`, and you can create it if you have all the needed ingredients from `ingredients[i]`. A recipe can also be an ingredient for other recipes, i.e., `ingredients[i]` may contain a string that is in `recipes`.
-* You are also given a string array `supplies` containing all the ingredients that you initially have, and you have an infinite supply of all of them.
-* Return a list of all the recipes that you can create. You may return the answer in any order.
+* You have information about `n` different recipes. You are given a string array `recipes` 
+* _ and a 2D string array `ingredients`. The `i^th` recipe has the name `recipes[i]`, and 
+* _ you can create it if you have all the needed ingredients from `ingredients[i]`. 
+* _ A recipe can also be an ingredient for other recipes, i.e., `ingredients[i]` may contain 
+* _ a string that is in `recipes`.
+* You are also given a string array `supplies` containing all the ingredients that you 
+* _ initially have, and you have an infinite supply of all of them.
+* Return a list of all the recipes that you can create. 
+* _ You may return the answer in any order.
 * Note that two recipes may contain each other in their ingredients.
 *
 * Example 1:
