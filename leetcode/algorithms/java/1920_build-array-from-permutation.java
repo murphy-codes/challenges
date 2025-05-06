e66b40c4169792ad2f157bb47c4b1942353db198
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-05-05 19:54:30 -0700

    Updated leetcode 1920_build-array-from-permutation.java solution

--- leetcode/algorithms/java/1920_build-array-from-permutation.java
+++ leetcode/algorithms/java/1920_build-array-from-permutation.java
@@ -2,8 +2,8 @@
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-05-05
 // At the time of submission:
-//   Runtime 2 ms Beats 7.30%
-//   Memory 45.55 MB Beats 27.48%
+//   Runtime 0 ms Beats 100.00%
+//   Memory 45.39 MB Beats 63.62%
 
 /****************************************
 * 
@@ -39,24 +39,16 @@
 ****************************************/
 
 class Solution {
-    // Encode the new value nums[nums[i]] at index i without using extra space.
-    // We do this by storing both the current and new value in a single integer:
-    // nums[i] = old_value + (new_value % n) * n.
-    // In the second pass, we retrieve the new value using nums[i] / n.
-    // Time Complexity: O(n), Space Complexity: O(1) (in-place modification).
     public int[] buildArray(int[] nums) {
-        int n = nums.length;
-
-        // Encode new values into the existing array
-        for (int i = 0; i < n; i++) {
-            nums[i] += (nums[nums[i]] % n) * n;
-        }
-
-        // Decode to get the final result
-        for (int i = 0; i < n; i++) {
-            nums[i] /= n;
-        }
-
+        apermutation(nums,0);
         return nums;
+    }
+    void apermutation(int[] nums, int start){
+        if(start<nums.length){
+            int temp = nums[start];
+            int result = nums[temp];
+            apermutation(nums,start+1);
+            nums[start]=result;
+        }
     }
 }
