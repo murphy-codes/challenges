e33a6c5b5e5e51b489c007f76f9309ea85e2e32f
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-07-22 16:58:40 -0700

    Add leetcode 802_find-eventual-safe-states.java solution

--- leetcode/algorithms/java/802_find-eventual-safe-states.java
+++ leetcode/algorithms/java/802_find-eventual-safe-states.java
@@ -1,17 +1,25 @@
 // Source: https://leetcode.com/problems/find-eventual-safe-states/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-01-23
+// At the time of submission:
+//   Runtime 26 ms Beats 24.92%
+//   Memory 55.06 MB Beats 96.93%
 
 /****************************************
 * 
-* There is a directed graph of `n` nodes with each node labeled from `0` to `n - 1`. The graph is represented by a 0-indexed 2D integer array `graph` where `graph[i]` is an integer array of nodes adjacent to node `i`, meaning there is an edge from node `i` to each node in `graph[i]`.
-* 
-* A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
-* 
-* Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+* 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+* There is a directed graph of `n` nodes with each node labeled from `0` to `n - 1`. 
+* _ The graph is represented by a 0-indexed 2D integer array `graph` where `graph[i]` 
+* _ is an integer array of nodes adjacent to node `i`, meaning there is an edge 
+* _ from node `i` to each node in `graph[i]`.
+* A node is a terminal node if there are no outgoing edges. A node is a safe node 
+* _ if every possible path starting from that node leads to a terminal node 
+* _ (or another safe node).
+* Return an array containing all the safe nodes of the graph. The answer should be 
+* _ sorted in ascending order.
 * 
 * Example 1:
-*   [Illustration of graph: https://s3-lc-upload.s3.amazonaws.com/uploads/2018/03/17/picture1.png]
+*   [Image: https://s3-lc-upload.s3.amazonaws.com/uploads/2018/03/17/picture1.png]
 * Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
 * Output: [2,4,5,6]
 * Explanation: The given graph is shown above.
