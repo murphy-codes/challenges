a17b27a54d256913522cd9644f2369c747856dbe
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-07-22 15:02:12 -0700

    Add leetcode 3_longest-substring-without-repeating-characters.java solution

--- leetcode/algorithms/java/3_longest-substring-without-repeating-characters.java
+++ leetcode/algorithms/java/3_longest-substring-without-repeating-characters.java
@@ -1,6 +1,9 @@
 // Source: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2024-12-27
+// At the time of submission:
+//   Runtime 6 ms Beats 72.64%
+//   Memory 44.92 MB Beats 31.87%
 
 /****************************************
 * 
@@ -23,9 +26,8 @@
 * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 *  
 * Constraints:
-* 
-* 0 <= s.length <= 5 * 104
-* s consists of English letters, digits, symbols and spaces.
+* • 0 <= s.length <= 5 * 104
+* • s consists of English letters, digits, symbols and spaces.
 * 
 ****************************************/
 
