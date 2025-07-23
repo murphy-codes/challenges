b47bdf17001c2b4609551996b0a3ceb1c12ce02b
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-07-22 17:12:59 -0700

    Add leetcode 2493_divide-nodes-into-the-maximum-number-of-groups.java solution

--- leetcode/algorithms/java/2493_divide-nodes-into-the-maximum-number-of-groups.java
+++ leetcode/algorithms/java/2493_divide-nodes-into-the-maximum-number-of-groups.java
@@ -1,22 +1,28 @@
 // Source: https://leetcode.com/problems/divide-nodes-into-the-maximum-number-of-groups/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-01-29
+// At the time of submission:
+//   Runtime 655 ms Beats 29.63%
+//   Memory 47.98 MB Beats 75.93%
 
 /****************************************
 * 
-* You are given a positive integer n representing the number of nodes in an undirected graph. The nodes are labeled from `1` to `n.
-* 
-* You are also given a 2D integer array `edges`, where `edges[i] = [a_i, b_i]` indicates that there is a bidirectional edge between nodes `a_i` and `b_i`. Notice that the given graph may be disconnected.
-* 
+* You are given a positive integer `n` representing the number of nodes in an 
+* _ undirected graph. The nodes are labeled from `1` to `n.
+* You are also given a 2D integer array `edges`, where `edges[i] = [a_i, b_i]` 
+* _ indicates that there is a bidirectional edge between nodes `a_i` and `b_i`. 
+* _ Notice that the given graph may be disconnected.
 * Divide the nodes of the graph into `m` groups (1-indexed) such that:
-* 
-* • Each node in the graph belongs to exactly one group.
-* • For every pair of nodes in the graph that are connected by an edge `[a_i, b_i]`, if `a_i` belongs to the group with index `x`, and `b_i` belongs to the group with index `y`, then `|y - x| = 1`.
-* 
-* Return the maximum number of groups (i.e., maximum `m`) into which you can divide the nodes. Return `-1` if it is impossible to group the nodes with the given conditions.
+* _ • Each node in the graph belongs to exactly one group.
+* _ • For every pair of nodes in the graph that are connected by an edge 
+* __ `[a_i, b_i]`, if `a_i` belongs to the group with index `x`, and `b_i` 
+* __ belongs to the group with index `y`, then `|y - x| = 1`.
+* Return the maximum number of groups (i.e., maximum `m`) into which you can 
+* _ divide the nodes. Return `-1` if it is impossible to group the nodes with 
+* _ the given conditions.
 * 
 * Example 1:
-*   [Image depicting 4 groups, with G1 having 5, which connects to 1 in G2, which then connects to both 2 & 4 in G3. Finally, 2 in G3 connects to both 3 & 6 in G4, but 4 in G3 only connects to 6 in G4 (thus 6 in G4 connects to both elements in G3). https://assets.leetcode.com/uploads/2022/10/13/example1.png]
+*   [Image: https://assets.leetcode.com/uploads/2022/10/13/example1.png]
 * Input: n = 6, edges = [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]]
 * Output: 4
 * Explanation: As shown in the image we:
@@ -25,12 +31,15 @@
 * - Add nodes 2 and 4 to the third group.
 * - Add nodes 3 and 6 to the fourth group.
 * We can see that every edge is satisfied.
-* It can be shown that that if we create a fifth group and move any node from the third or fourth group to it, at least on of the edges will not be satisfied.
+* It can be shown that that if we create a fifth group and move any node from the 
+* _ third or fourth group to it, at least on of the edges will not be satisfied.
 * 
 * Example 2:
 * Input: n = 3, edges = [[1,2],[2,3],[3,1]]
 * Output: -1
-* Explanation: If we add node 1 to the first group, node 2 to the second group, and node 3 to the third group to satisfy the first two edges, we can see that the third edge will not be satisfied.
+* Explanation: If we add node 1 to the first group, node 2 to the second group, 
+* _ and node 3 to the third group to satisfy the first two edges, we can see that 
+* _ the third edge will not be satisfied.
 * It can be shown that no grouping is possible.
 * 
 * Constraints:
