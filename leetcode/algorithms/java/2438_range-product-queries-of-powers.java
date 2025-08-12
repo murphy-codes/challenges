8d431d17f4f6fcbec6196ecdc3cbd86839d099db
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-08-11 20:56:28 -0700

    Add leetcode 2438_range-product-queries-of-powers.java solution

--- leetcode/algorithms/java/2438_range-product-queries-of-powers.java
+++ leetcode/algorithms/java/2438_range-product-queries-of-powers.java
@@ -1,31 +1,43 @@
 // Source: https://leetcode.com/problems/range-product-queries-of-powers/
 // Author: Tom Murphy https://github.com/murphy-codes/
-// Date: 2025-08-08
+// Date: 2025-08-11
 // At the time of submission:
 //   Runtime 3 ms Beats 100.00%
 //   Memory 89.55 MB Beats 44.09%
 
 /****************************************
 * 
-* Given an integer `n`, return `true` if it is a power of two. Otherwise, return `false`.
-* An integer `n` is a power of two, if there exists an integer `x` such that `n == 2^x`.
+* Given a positive integer `n`, there exists a 0-indexed array called `powers`,
+* _ composed of the minimum number of powers of `2` that sum to `n`. The array is
+* _ sorted in non-decreasing order, and there is only one way to form the array.
+* You are also given a 0-indexed 2D integer array `queries`, where
+* _ `queries[i] = [left_i, right_i]`. Each `queries[i]` represents a query where
+* _ you have to find the product of all `powers[j]` with `left_i <= j <= right_i`.
+* Return an array `answers`, equal in length to `queries`, where `answers[i]` is
+* _ the answer to the `i^th` query. Since the answer to the `i^th` query may be
+* _ too large, each `answers[i]` should be returned modulo `10^9 + 7`.
 *
 * Example 1:
-* Input: n = 1
-* Output: true
-* Explanation: 2^0 = 1
+* Input: n = 15, queries = [[0,1],[2,2],[0,3]]
+* Output: [2,4,64]
+* Explanation:
+* For n = 15, powers = [1,2,4,8]. It can be shown that powers cannot be a smaller size.
+* Answer to 1st query: powers[0] * powers[1] = 1 * 2 = 2.
+* Answer to 2nd query: powers[2] = 4.
+* Answer to 3rd query: powers[0] * powers[1] * powers[2] * powers[3] = 1 * 2 * 4 * 8 = 64.
+* Each answer modulo 109 + 7 yields the same answer, so [2,4,64] is returned.
 *
 * Example 2:
-* Input: n = 16
-* Output: true
-* Explanation: 2^4 = 16
-*
-* Example 3:
-* Input: n = 3
-* Output: false
+* Input: n = 2, queries = [[0,0]]
+* Output: [2]
+* Explanation:
+* For n = 2, powers = [2].
+* The answer to the only query is powers[0] = 2. The answer modulo 109 + 7 is the same, so [2] is returned.
 *
 * Constraints:
-* • -2^31 <= n <= 2^31 - 1
+* • 1 <= n <= 10^9
+* • 1 <= queries.length <= 10^5
+* • 0 <= start_ <= end_i < powers.length
 * 
 ****************************************/
 
