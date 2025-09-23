9ec9825b4ad9815f9c2f2740aee48e6032b1b574
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-09-23 16:19:07 -0700

    Updated leetcode 3042_count-prefix-and-suffix-pairs-i.java solution

--- leetcode/algorithms/java/3042_count-prefix-and-suffix-pairs-i.java
+++ leetcode/algorithms/java/3042_count-prefix-and-suffix-pairs-i.java
@@ -1,16 +1,22 @@
 // Source: https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-01-07
+// At the time of submission:
+//   Runtime 2 ms Beats 100.00%
+//   Memory 42.10 MB Beats 88.82%
 
 /****************************************
 * 
-* You are given a 0-indexed string array words.
-* 
-* Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
-* • isPrefixAndSuffix(str1, str2) returns true if str1 is both a prefix and a suffix of str2, and false otherwise.
-* For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
-* 
-* Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+* You are given a 0-indexed string array `words`.
+* Let's define a boolean function `isPrefixAndSuffix` that takes two strings, 
+* _ `str1` and `str2`:
+* • `isPrefixAndSuffix(str1, str2)` returns `true` if `str1` is both a prefix 
+* __ and a suffix of `str2`, and `false` otherwise.
+* For example, `isPrefixAndSuffix("aba", "ababa")` is `true` because `"aba"` is 
+* _ a prefix of `"ababa"` and also a suffix, but 
+* _ `isPrefixAndSuffix("abc", "abcd")` is `false`.
+* Return an integer denoting the number of index pairs `(i, j)` such that 
+* _`i < j`, and `isPrefixAndSuffix(words[i], words[j])` is `true`.
 * 
 * Example 1:
 * Input: words = ["a","aba","ababa","aa"]
@@ -53,7 +59,6 @@ class Solution {
     public int countPrefixSuffixPairs(String[] words) {
         int count = 0;
         int n = words.length;
-
         // Iterate over all pairs (i, j) where i < j
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -63,7 +68,6 @@ class Solution {
                 }
             }
         }
-
         return count;
     }
 }
