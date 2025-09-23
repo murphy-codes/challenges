7e5aa77c79a053d91fc33039066c59d7b16e4a6a
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2025-09-23 16:53:40 -0700

    Add leetcode 2560_house-robber-iv.java solution

--- leetcode/algorithms/java/2560_house-robber-iv.java
+++ leetcode/algorithms/java/2560_house-robber-iv.java
@@ -1,14 +1,23 @@
 // Source: https://leetcode.com/problems/house-robber-iv/
 // Author: Tom Murphy https://github.com/murphy-codes/
 // Date: 2025-01-16
+// At the time of submission:
+//   Runtime 18 ms Beats 89.39%
+//   Memory 65.55 MB Beats 5.65%
 
 /****************************************
 * 
-* There are several consecutive houses along a street, each of which has some money inside. There is also a robber, who wants to steal money from the homes, but he refuses to steal from adjacent homes.
-* The capability of the robber is the maximum amount of money he steals from one house of all the houses he robbed.
-* You are given an integer array `nums` representing how much money is stashed in each house. More formally, the `i^th` house from the left has `nums[i]` dollars.
-* You are also given an integer `k`, representing the minimum number of houses the robber will steal from. It is always possible to steal at least `k` houses.
-* Return the minimum capability of the robber out of all the possible ways to steal at least `k` houses.
+* There are several consecutive houses along a street, each of which has some 
+* _ money inside. There is also a robber, who wants to steal money from the 
+* _ homes, but he refuses to steal from adjacent homes.
+* The capability of the robber is the maximum amount of money he steals from one 
+* _ house of all the houses he robbed.
+* You are given an integer array `nums` representing how much money is stashed in 
+* _ each house. More formally, the `i^th` house from the left has `nums[i]` dollars.
+* You are also given an integer `k`, representing the minimum number of houses 
+* _ the robber will steal from. It is always possible to steal at least `k` houses.
+* Return the minimum capability of the robber out of all the possible ways to 
+* _ steal at least `k` houses.
 *
 * Example 1:
 * Input: nums = [2,3,5,9], k = 2
