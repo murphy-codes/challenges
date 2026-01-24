ddbd6430e07eb1643e70d8189233ddceb5fb0e92
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2026-01-23 19:38:47 -0800

    Add leetcode 1877_minimize-maximum-pair-sum-in-array.java solution

---
+++ leetcode/algorithms/java/1877_minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,52 @@
+// Source: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+// Author: Tom Murphy https://github.com/murphy-codes/
+// Date: 2026-01-23
+// At the time of submission:
+//   Runtime 58 ms Beats 63.10%
+//   Memory 97.08 MB Beats 40.09%
+
+/****************************************
+* 
+* The pair sum of a pair `(a,b)` is equal to `a + b`. The maximum pair sum is
+* _ the largest pair sum in a list of pairs.
+* • For example, if we have pairs `(1,5)`, `(2,3)`, and `(4,4)`, the maximum
+* __ pair sum would be `max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8`.
+* Given an array `nums` of even length `n`, pair up the elements of `nums`
+* _ into `n / 2` pairs such that:
+* • Each element of `nums` is in exactly one pair, and
+* • The maximum pair sum is minimized.
+* Return the minimized maximum pair sum after optimally pairing up the elements.
+*
+* Example 1:
+* Input: nums = [3,5,2,3]
+* Output: 7
+* Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+* The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+*
+* Example 2:
+* Input: nums = [3,5,4,2,4,6]
+* Output: 8
+* Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+* The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+*
+* Constraints:
+* • `n == nums.length`
+* • `2 <= n <= 10^5`
+* • `n` is even.
+* • `1 <= nums[i] <= 10^5`
+* 
+****************************************/
+
+import java.util.Arrays;
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        int minMax = 0;
+        Arrays.sort(nums);
+        int j = nums.length-1;
+        for (int i = 0; i < j; i++) {
+            minMax = Math.max(minMax, nums[i]+nums[j--]);
+        }
+        return minMax;
+    }
+}
