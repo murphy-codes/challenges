5b21ed1905f9a541adc7338a9ec7052e01d56180
murphy-codes <56618913+murphy-codes@users.noreply.github.com>
2026-05-23 02:16:43 -0700

    Add leetcode 1752_check-if-array-is-sorted-and-rotated.java solution

--- leetcode/algorithms/java/1752_check-if-array-is-sorted-and-rotated.java
+++ leetcode/algorithms/java/1752_check-if-array-is-sorted-and-rotated.java
@@ -1,35 +1,39 @@
 // Source: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
 // Author: Tom Murphy https://github.com/murphy-codes/
-// Date: 2025-02-02
+// Date: 2026-02-02
+// At the time of submission:
+//   Runtime 0 ms Beats 100.00%
+//   Memory 41.52 MB Beats 99.96%
 
 /****************************************
 * 
-* Given an array `nums`, return `true` if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return `false`.
-* 
+* Given an array `nums`, return `true` if the array was originally sorted in
+* _ non-decreasing order, then rotated some number of positions
+* _ (including zero). Otherwise, return `false`.
 * There may be duplicates in the original array.
-* 
-* Note: An array `A` rotated by `x` positions results in an array `B` of the same length such that `A[i] == B[(i+x) % A.length]`, where `%` is the modulo operation.
-* 
+* Note: An array `A` rotated by `x` positions results in an array `B` of the
+* _ same length such that `B[i] == A[(i+x) % A.length]` for every valid index `i`.
+*
 * Example 1:
 * Input: nums = [3,4,5,1,2]
 * Output: true
 * Explanation: [1,2,3,4,5] is the original sorted array.
-* You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
-* 
+* You can rotate the array by x = 2 positions to begin on the element of value 3: [3,4,5,1,2].
+*
 * Example 2:
 * Input: nums = [2,1,3,4]
 * Output: false
 * Explanation: There is no sorted array once rotated that can make nums.
-* 
+*
 * Example 3:
 * Input: nums = [1,2,3]
 * Output: true
 * Explanation: [1,2,3] is the original sorted array.
 * You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
-* 
+*
 * Constraints:
-* • 1 <= nums.length <= 100
-* • 1 <= nums[i] <= 100
+* • `1 <= nums.length <= 100`
+* • `1 <= nums[i] <= 100`
 * 
 ****************************************/
 
